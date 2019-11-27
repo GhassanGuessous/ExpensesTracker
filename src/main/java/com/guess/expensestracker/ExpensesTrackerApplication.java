869bdec8791a3d8b3907f7b27da98dba14f3e2ee
Ghassan <ghassanguessous1@gmail.com>
2019-11-27 12:27:13 +0100

    Initialize project with basic dependencies & a simple hello world rest controller

---
+++ src/main/java/com/guess/expensestracker/ExpensesTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.guess.expensestracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpensesTrackerApplication.class, args);
+    }
+
+}
