723f34ebd006f6f9e69ba2314eaf0efb83958082
Ghassan <ghassanguessous1@gmail.com>
2019-12-16 12:33:00 +0100

    CRUD of entity : Expense

--- src/main/java/com/guess/expensestracker/service/impl/DefaultExpenseService.java
+++ src/main/java/com/guess/expensestracker/service/impl/DefaultExpenseService.java
@@ -1,7 +1,51 @@
 package com.guess.expensestracker.service.impl;
 
+import com.guess.expensestracker.entity.Expense;
+import com.guess.expensestracker.repository.ExpenseRepository;
 import com.guess.expensestracker.service.ExpenseService;
+import com.guess.expensestracker.service.util.DateUtil;
+import com.guess.expensestracker.web.rest.errors.BadRequestAlertException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class DefaultExpenseService implements ExpenseService {
 
+    private final Logger log = LoggerFactory.getLogger(DefaultExpenseService.class);
+
+    private static final String ENTITY_NAME = "expense";
+
+    @Autowired
+    private ExpenseRepository expenseRepository;
+
+    @Override
+    public Expense save(Expense expense) {
+        try {
+            expense.setInitialDate(DateUtil.getCurrentDate());
+            return expenseRepository.save(expense);
+        } catch (ParseException e) {
+            throw new BadRequestAlertException("Invalid date", ENTITY_NAME);
+        }
+    }
+
+    @Override
+    public List<Expense> findAll() {
+        return expenseRepository.findAll();
+    }
+
+    @Override
+    public Optional<Expense> findOne(Long id) {
+        return expenseRepository.findById(id);
+    }
+
+    @Override
+    public void delete(Long id) {
+        expenseRepository.deleteById(id);
+    }
 }
--- src/main/java/com/guess/expensestracker/web/rest/ExpenseResource.java
+++ src/main/java/com/guess/expensestracker/web/rest/ExpenseResource.java
@@ -1,9 +1,17 @@
 package com.guess.expensestracker.web.rest;
 
+import com.guess.expensestracker.entity.Expense;
+import com.guess.expensestracker.service.ExpenseService;
+import com.guess.expensestracker.web.rest.errors.BadRequestAlertException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -11,4 +19,53 @@ public class ExpenseResource {
 
     private final Logger log = LoggerFactory.getLogger(ExpenseResource.class);
 
+    private static final String ENTITY_NAME = "expense";
+
+    @Autowired
+    private ExpenseService expenseService;
+
+    @PostMapping("/expenses")
+    ResponseEntity<Expense> createIncome(@RequestBody Expense expense) {
+        log.debug("REST request to save Expense : {}", expense);
+        if(expense.getId() != null) {
+            throw new BadRequestAlertException("A new expense cannot have already an Id!", ENTITY_NAME);
+        }
+        Expense result = expenseService.save(expense);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @PutMapping("/expenses")
+    ResponseEntity<Expense> updateIncome(@RequestBody Expense expense) {
+        log.debug("REST request to update Expense : {}", expense);
+        if(expense.getId() == null) {
+            throw new BadRequestAlertException("Invalid Id!", ENTITY_NAME);
+        }
+        Expense result = expenseService.save(expense);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @GetMapping("/expenses")
+    ResponseEntity<List<Expense>> getAllIncomes() {
+        log.debug("REST request to get all expenses");
+        List<Expense> expenses = expenseService.findAll();
+        return ResponseEntity.ok().body(expenses);
+    }
+
+    @GetMapping("/expenses/{id}")
+    ResponseEntity<Expense> getIncome(@PathVariable Long id) {
+        log.debug("REST request to get Expense : {}", id);
+        Optional<Expense> expense = expenseService.findOne(id);
+        if(expense.isPresent()) {
+            return ResponseEntity.ok().body(expense.get());
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/expenses/{id}")
+    ResponseEntity<Void> deleteIncome(@PathVariable Long id) {
+        log.debug("REST request to delete Expense : {}", id);
+        expenseService.delete(id);
+        return ResponseEntity.ok().build();
+    }
 }
